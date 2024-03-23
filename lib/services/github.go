cce19eb52616b3354df9c464190ef6421496d6d5
Andrew Aubury <andrewaubury1@gmail.com>
2024-03-23 10:32:31 +0000

    Remove ent check for github

--- lib/services/github.go
+++ lib/services/github.go
@@ -182,10 +182,7 @@ func MarshalOSSGithubConnector(githubConnector types.GithubConnector, opts ...Ma
 		// Only return an error if the endpoint url is set and the build is OSS
 		// so that the enterprise marshaler can call this marshaler to produce
 		// the final output without receiving an error.
-		if modules.GetModules().BuildType() == modules.BuildOSS &&
-			githubConnector.Spec.EndpointURL != "" {
-			return nil, fmt.Errorf("GitHub endpoint URL is set: %w", ErrRequiresEnterprise)
-		}
+
 		return utils.FastMarshal(maybeResetProtoResourceID(cfg.PreserveResourceID, githubConnector))
 	default:
 		return nil, trace.BadParameter("unrecognized github connector version %T", githubConnector)
